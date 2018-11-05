2f8abe7f1125af4bf9013ec3d6cf1ba571d2415d
Harsh Agrawal <harshagrawal007@gmail.com>
2018-11-04 19:15:17 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ connection.once('open', function callback() {
 
 //Allow Access Control
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://ec2-18-191-107-23.us-east-2.compute.amazonaws.com:3000');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
